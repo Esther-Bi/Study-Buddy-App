5fb6bd67c948548fe7edb7ada479e754afb948c7
Esther Binnes <estherbinnes@gmail.com>
2022-12-06 19:33:28 +0200

    add student home and choose user

---
+++ app/src/main/java/com/example/studybuddy/ChooseUserActivity.java
@@ -0,0 +1,46 @@
+package com.example.studybuddy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+
+public class ChooseUserActivity extends AppCompatActivity {
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference coursesRef = db.collection("courses");
+
+
+    private FirebaseAuth auth;
+    private FirebaseFirestore database;
+    Button sign_as_student;
+    Button sign_as_teacher;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_user);
+
+        sign_as_student = findViewById(R.id.sign_as_student);
+        sign_as_teacher = findViewById(R.id.sign_as_teacher);
+
+        sign_as_student.setOnClickListener(v -> {
+            startActivity(new Intent(ChooseUserActivity.this, StudentHomeActivity.class));
+        });
+
+        sign_as_teacher.setOnClickListener(v -> {
+            startActivity(new Intent(ChooseUserActivity.this, HomeActivity.class));
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/studybuddy/StudentHomeActivity.java
@@ -0,0 +1,70 @@
+package com.example.studybuddy;
+
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class StudentHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_edit_profile:
+                startActivity(new Intent(StudentHomeActivity.this, ProfileActivity.class));
+                finish();
+                return true;
+            case R.id.action_book_a_class:
+                Toast.makeText(this, "Item 2 selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_payments:
+                Toast.makeText(this, "Item 3 selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_my_courses:
+                startActivity(new Intent(StudentHomeActivity.this, MyCoursesActivity.class));
+                finish();
+                return true;
+            case R.id.action_log_out:
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(StudentHomeActivity.this, MainActivity.class));
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
