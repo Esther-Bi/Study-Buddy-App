3104d62e6e4b8a586ce61eaab1bd5c6cb2cd484f
Esther Binnes <estherbinnes@gmail.com>
2023-01-05 16:34:33 +0200

    update my courses page for adding and deleting

--- app/src/main/java/com/example/studybuddy/MyCoursesActivity.java
+++ app/src/main/java/com/example/studybuddy/MyCoursesActivity.java
@@ -4,14 +4,23 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.content.Intent;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -27,6 +36,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
@@ -36,119 +46,200 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.firestore.SetOptions;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
 public class MyCoursesActivity extends AppCompatActivity {
 
-    private FirebaseFirestore db = FirebaseFirestore.getInstance();
-    private CollectionReference coursesRef = db.collection("courses");
-
-
-    private FirebaseAuth auth;
-    private FirebaseFirestore database;
-    Button add;
-    EditText course, grade;
-    private CourseAdapter adapter;
-
     GoogleSignInClient googleSignInClient;
 
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference teachersRef = db.collection("teachers");
+    private static final String TAG = "MyCoursesActivity";
+
+    private AlertDialog.Builder dialogBuilder;
+    private AlertDialog dialog;
+    private Button popup_course_cancel , popup_course_save;
+    private EditText popup_course, popup_grade;
+    private String currentID;
+
+    Button add_courses;
+    ListView resultsListView;
+    List<HashMap<String, String>> listItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_courses);
 
-        setUpRecyclerView();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        currentID = user.getUid();
 
-        googleSignInClient= GoogleSignIn.getClient(MyCoursesActivity.this, GoogleSignInOptions.DEFAULT_SIGN_IN);
+        resultsListView = (ListView) findViewById(R.id.results_listview);
+        int[] colors = {0, 0, 0}; // red for the example
+        resultsListView.setDivider(new GradientDrawable(GradientDrawable.Orientation.RIGHT_LEFT, colors));
+        resultsListView.setDividerHeight(50);
 
-        auth = FirebaseAuth.getInstance();
-        database = FirebaseFirestore.getInstance();
-        FirebaseUser user = auth.getCurrentUser();
+        add_courses = findViewById(R.id.add_courses);
 
-        assert user != null;
-        String userUID = user.getUid();
+        setData();
 
-        add = findViewById(R.id.add_course);
-        course = findViewById(R.id.course_to_add);
-        grade = findViewById(R.id.grade_to_add);
+    }
 
-        //onclick listener for register button
-        add.setOnClickListener(v -> {
-            //Converting fields to text
-            String textCourse = course.getText().toString();
-            String textGrade = grade.getText().toString();
+    private void setData() {
+        teachersRef.whereEqualTo("id", currentID).get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
 
-            if (TextUtils.isEmpty(textCourse) || TextUtils.isEmpty(textGrade)) {
-                Toast.makeText(MyCoursesActivity.this, "Empty Credentials!", Toast.LENGTH_SHORT).show();
-            } else {
-//                ArrayList<String> courses_list = new ArrayList<String>();
-//                ArrayList<Integer> grades_list = new ArrayList<Integer>();
+                        for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
+                            Teacher teacher = documentSnapshot.toObject(Teacher.class);
+                            String[] courses = teacher.getCourses().toArray((new String[teacher.getCourses().size()]));
+                            Integer[] grades = teacher.getGrades().toArray((new Integer[teacher.getGrades().size()]));
 
-                // Retrieve the existing array from Firestore
-//                database.collection("teachers")
-//                        .document(userUID)
-//                        .get()
-//                        .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-//                            @Override
-//                            public void onSuccess(DocumentSnapshot documentSnapshot) {
-//                                courses_list = (ArrayList<String>) documentSnapshot.get("courses");
-//                                grades_list = (ArrayList<Integer>) documentSnapshot.get("grades");
-//                            }
-//                        });
-//
-//                // Add a new value to the array
-                database.collection("teachers")
-                        .document(userUID)
-                        .update("courses", FieldValue.arrayUnion(textCourse));
+                            HashMap<String, String> course_and_grade = new HashMap<>();
+                            for (int i = 0; i < teacher.getGrades().size(); i++) {
+                                course_and_grade.put(courses[i], String.valueOf(grades[i]));
+                            }
+                            IterateData(course_and_grade);
+                            setUpOnclickListener();
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG, e.toString());
+                    }
+                });
 
-                database.collection("teachers")
-                        .document(userUID)
-                        .update("grades", FieldValue.arrayUnion(Integer.parseInt(textGrade)));
+        add_courses.setOnClickListener(v -> {
+            createCoursePopup();
+        });
 
-                Toast.makeText(MyCoursesActivity.this, "Course Have Been Added", Toast.LENGTH_SHORT).show();
+    }
+
+    private void IterateData(HashMap<String, String> course_and_grade){
+
+        listItems = new ArrayList<>();
+        SimpleAdapter adapter = new SimpleAdapter(this, listItems, R.layout.course_item,
+                new String[]{"Course", "Grade"},
+                new int[]{R.id.course, R.id.grade});
+
+        Iterator it = course_and_grade.entrySet().iterator();
+        while (it.hasNext()) {
+            HashMap<String, String> resultsMap = new HashMap<>();
+            Map.Entry pair = (Map.Entry)it.next();
+            resultsMap.put("Course", pair.getKey().toString());
+            resultsMap.put("Grade", pair.getValue().toString());
+            listItems.add(resultsMap);
+        }
+
+        resultsListView.setAdapter(adapter);
+    }
+
+    public void createCoursePopup(){
+        dialogBuilder = new AlertDialog.Builder(this);
+        final View coursePopupView = getLayoutInflater().inflate(R.layout.courses_popup , null);
+        popup_course = (EditText) coursePopupView.findViewById(R.id.popup_course);
+        popup_grade = (EditText) coursePopupView.findViewById(R.id.popup_grade);
+        popup_course_cancel = (Button) coursePopupView.findViewById(R.id.popup_course_cancel);
+        popup_course_save = (Button) coursePopupView.findViewById(R.id.popup_course_save);
+
+        dialogBuilder.setView(coursePopupView);
+        dialog = dialogBuilder.create();
+        dialog.show();
+
+        popup_course_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
             }
         });
 
+        popup_course_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String course = popup_course.getText().toString();
+                String grade = popup_grade.getText().toString();
+                if (TextUtils.isEmpty(course) || TextUtils.isEmpty(grade)) {
+                    Toast.makeText(MyCoursesActivity.this, "Empty Credentials!", Toast.LENGTH_SHORT).show();
+                } else {
+                    String course_and_grade = course + " - " + grade;
+                    db.collection("teachers")
+                            .document(currentID)
+                            .update("courses", FieldValue.arrayUnion(course));
+                    db.collection("teachers")
+                            .document(currentID)
+                            .update("grades", FieldValue.arrayUnion(Integer.parseInt(grade)));
+
+                    Toast.makeText(MyCoursesActivity.this, course_and_grade + " have been added successfully", Toast.LENGTH_SHORT).show();
+                    setData();
+                    dialog.dismiss();
+                }
+            }
+        });
 
     }
 
-    private void setUpRecyclerView() {
-
-        auth = FirebaseAuth.getInstance();
-        FirebaseUser user = auth.getCurrentUser();
-
-        assert user != null;
-        String userUID = user.getUid();
-
-        Query query = coursesRef;
-        FirestoreRecyclerOptions<Course> options = new FirestoreRecyclerOptions.Builder<Course>()
-                .setQuery(query, Course.class)
-                .build();
-
-        adapter = new CourseAdapter(options);
-
-        RecyclerView recyclerView = findViewById(R.id.recycler_view);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(adapter);
+    private void setUpOnclickListener()
+    {
+        resultsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l)
+            {
+                HashMap<String, String> item = (HashMap<String, String>) resultsListView.getItemAtPosition(position);
+                deleteQuestionPopup(item.get("Course") , item.get("Grade"));
+            }
+        });
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        adapter.startListening();
-    }
+    public void deleteQuestionPopup(String course, String grade) {
+        dialogBuilder = new AlertDialog.Builder(this);
+        final View popupView = getLayoutInflater().inflate(R.layout.delete_question_popup, null);
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        adapter.stopListening();
+        TextView question = popupView.findViewById(R.id.question);
+        Button yes_button = popupView.findViewById(R.id.yes_button);
+        Button no_button = popupView.findViewById(R.id.no_button);
+        question.setText("Are you sure you want to delete\n" + course + " course?");
+
+        dialogBuilder.setView(popupView);
+        dialog = dialogBuilder.create();
+        dialog.show();
+
+        yes_button.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("LongLogTag")
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+                Log.d(TAG, "delete course");
+                db.collection("teachers")
+                        .document(currentID)
+                        .update("courses", FieldValue.arrayRemove(course));
+                int grade_int = Integer.parseInt(grade);
+                db.collection("teachers")
+                        .document(currentID)
+                        .update("grades", FieldValue.arrayRemove(grade_int));
+                Toast.makeText(MyCoursesActivity.this, "course have been deleted successfully", Toast.LENGTH_SHORT).show();
+                setData();
+            }
+        });
+
+        no_button.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("LongLogTag")
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "don't delete course");
+                dialog.dismiss();
+            }
+        });
+
     }
 
 
